ebd5a350f0f0de1cc699f4008d914142c37c1440
JenniferZvonek <jezvonek@gmail.com>
2021-08-05 17:14:57 -0400

    more bugs

--- Extended_DMD.py
+++ Extended_DMD.py
@@ -109,8 +109,8 @@ K = np.load('./DeepDMD_Weights/K.npy') #load K from deep DMD loss
 ## compute K_dmd using extended DMD with the neural network as the dictionary functions
 #construct matrix Theta
 num_seq = tf.shape(data_orig_stacked).numpy()[1]
-Theta_X = np.zeros(num_observables, num_seq)
-Theta_Y = np.zeros(num_observables, num_seq)
+Theta_X = np.zeros((num_observables, num_seq))
+Theta_Y = np.zeros((num_observables, num_seq))
 
 for i in range(num_seq):
     Theta_X[:,i] = model.predict(data_orig_stacked[0,i,:])
