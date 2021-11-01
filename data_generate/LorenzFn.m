e61371f6fa38da3d30a7d973e02c2401d03a192d
JenniferZvonek <jezvonek@gmail.com>
2021-10-31 21:51:18 -0400

    Wrote function LorenzFn.m, which solves the Lorenz system, and the script Lorenz_data.m, which calls LorenzFn and generates/saves Lorenz data for a given set of initial conditions and time range. Resulting data can be found in files containing the word "Lorenz" in the "data" directory.

---
+++ data_generate/LorenzFn.m
@@ -0,0 +1,33 @@
+function X = LorenzFn(xrange, yrange, zrange, numICs, tSpan, seed)
+
+    % set random number generator seed
+    rng(seed)
+
+    % set lorenz parameters
+    sigma = 10;
+    beta = 8/3;
+    rho = 28;
+
+    % define lorenz system
+    % note that xyz = [x; y; z]
+    lorenz_sys = @(t,xyz) [sigma*(xyz(2,:) - xyz(1,:)); 
+                     xyz(1,:).*(rho - xyz(3,:)) - xyz(2,:); 
+                     xyz(1,:).*xyz(2,:) - beta*xyz(3,:)];
+
+    lenT = length(tSpan);
+    X = zeros(numICs*lenT, 3);
+
+    % generate data for random initial conditions
+    for j = 1:numICs
+        % choose random initial conditions in xrange x yrange x zrange
+        x = (xrange(2) - xrange(1))*rand + xrange(1);
+        y = (yrange(2) - yrange(1))*rand + yrange(1);
+        z = (zrange(2) - zrange(1))*rand + zrange(1);
+
+        ic = [x, y, z];
+
+        % solve system and store data in X
+        [~, temp] = ode45(lorenz_sys, tSpan, ic);
+        X(1+(j-1)*lenT : lenT + (j-1)*lenT,:) = temp;
+    end
+end
---
+++ data_generate/Lorenz_data.m
@@ -0,0 +1,29 @@
+numICs = 5000;
+filenamePrefix = 'Lorenz';
+
+%set initial condition ranges (need to verify that these are good choices)
+xrange = [0,2];
+yrange = [0,2];
+zrange = [0,2];
+
+tSpan = 0:0.02:1;
+
+% generate test data (which is 10% the number of numICs)
+seed = 1;
+X_test = LorenzFn(xrange, yrange, zrange, round(.1*numICs), tSpan, seed);
+filename_test = strcat(filenamePrefix, '_test_x.csv');
+dlmwrite(filename_test, X_test, 'precision', '%.14f')
+
+% generate validation data (which is 20% the number of numICs)
+seed = 2;
+X_val = LorenzFn(xrange, yrange, zrange, round(.2*numICs), tSpan, seed);
+filename_val = strcat(filenamePrefix, '_val_x.csv');
+dlmwrite(filename_val, X_val, 'precision', '%.14f')
+
+% generate training data sets, each of which has 70% of numICs samples
+for j = 1:6
+	seed = 2+j;
+	X_train = LorenzFn(xrange, yrange, zrange, round(.7*numICs), tSpan, seed);
+	filename_train = strcat(filenamePrefix, sprintf('_train%d_x.csv', j));
+	dlmwrite(filename_train, X_train, 'precision', '%.14f')
+end
