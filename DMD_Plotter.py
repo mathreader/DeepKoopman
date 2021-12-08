ee170ad3061eb53f08dc2c3dd7851470983a3082
JenniferZvonek <jezvonek@gmail.com>
2021-12-08 11:46:58 -0500

    Modified DMD_Plotter.py to also plot the residual loss over time.

--- DMD_Plotter.py
+++ DMD_Plotter.py
@@ -1,18 +1,12 @@
 from matplotlib import pyplot
 import numpy as np
 
-#extension = 'experiment_1_10_2021_08_20_17_30_50_887276' #experiment 1
-#extension = 'experiment_2_10_2021_08_20_17_32_21_985846' #experiment 2
-#extension = 'experiment_4_10_2021_08_27_09_45_22_292512' #experiment 4
-
-# New ones 
-extension = 'experiment_4_10_2021_08_28_18_31_30_929630' #actually experiment 5, though forgot to change name, frobenius squared norm + condition number on K + spectral leakage
-
-# 
-extension = 'experiment_6_10_2021_08_29_16_17_09_063377'
+data_name = 'Lorenz1'
+experiment_tag = 'experiment_18'
+date = '2021_12_07_12_46_16_804693'
 
 ## Load Data
-f = open('./DeepDMD_results/Pendulum_' + extension + '_error.csv')
+f = open('./DeepDMD_results/' + data_name + '_' + experiment_tag + '_' + date + '_error.csv')
 f_lines = f.readlines()
 len_f = len(f_lines) - 1
 
@@ -20,25 +14,32 @@ len_f = len(f_lines) - 1
 data = {}
 data['epochs'] 			= np.zeros(len_f)
 data['times'] 			= np.zeros(len_f)
+
 data['train_MSE'] 		= np.zeros(len_f)
 data['train_cond_num'] 	= np.zeros(len_f)
 data['train_SL'] 		= np.zeros(len_f)
+data['train_res']		= np.zeros(len_f)
+
 data['val_MSE'] 		= np.zeros(len_f)
 data['val_cond_num'] 	= np.zeros(len_f)
 data['val_SL'] 			= np.zeros(len_f)
+data['val_res']			= np.zeros(len_f)
 
 for i in range(0,len_f):
-	print(i)
 	line = f_lines[i + 1].strip('\n').split(', ')
 
 	data['epochs'][i] 			= int(line[0]);
 	data['times'][i] 			= float(line[1]);
+
 	data['train_MSE'][i] 		= float(line[2]);
 	data['train_cond_num'][i] 	= float(line[3]);
 	data['train_SL'][i] 		= float(line[4]);
-	data['val_MSE'][i] 			= float(line[5]);
-	data['val_cond_num'][i] 	= float(line[6]);
-	data['val_SL'][i] 			= float(line[7]);
+	data['train_res'][i]		= float(line[5]);
+
+	data['val_MSE'][i] 			= float(line[6]);
+	data['val_cond_num'][i] 	= float(line[7]);
+	data['val_SL'][i] 			= float(line[8]);
+	data['val_res'][i]			= float(line[9]);
 
 ## Plot Results
 
@@ -47,8 +48,8 @@ pyplot.plot(data['times']/60, data['val_MSE'])
 pyplot.yscale('log')
 pyplot.xlabel('Time (min)')
 pyplot.ylabel('Prediction Validation Error')
-pyplot.title('Experiment with loss as prediction error + regularization on condition number')
-pyplot.savefig("./Plots/Prediction_Error_Experiment_4_10.png")
+pyplot.title(experiment_tag)
+pyplot.savefig("./Plots/Prediction_Error_{}.png".format(experiment_tag))
 pyplot.show()
 
 
@@ -57,6 +58,16 @@ pyplot.plot(data['times']/60, data['val_SL'])
 pyplot.yscale('log')
 pyplot.xlabel('Time (min)')
 pyplot.ylabel('Spectral Leakage Error')
-pyplot.title('Experiment with loss as prediction error + regularization on condition number')
-pyplot.savefig("./Plots/SL_Error_Experiment_4_10.png")
+pyplot.title(experiment_tag)
+pyplot.savefig("./Plots/SL_Error_{}.png".format(experiment_tag))
 pyplot.show()
+
+#plot residual over time
+pyplot.plot(data['times']/60, data['val_res'])
+pyplot.yscale('log')
+pyplot.xlabel('Time (min)')
+pyplot.ylabel('Residual')
+pyplot.title(experiment_tag)
+pyplot.savefig("./Plots/Residual_{}.png".format(experiment_tag))
+pyplot.show()
+
